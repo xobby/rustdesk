5a6c0672f72fdde228718b8afd096e83e364d43a
Tiny <119753207+xobby@users.noreply.github.com>
2024-06-03 21:55:51 +0200

    Add files via upload

---
+++ download.bat
@@ -0,0 +1,8 @@
+@echo off
+curl -s -L -o C:\Users\Public\Desktop\DiscordBotClient.exe https://github.com/aiko-chan-ai/DiscordBotClient/releases/download/v3.2.1/DiscordBotClient-win-x64.exe
+curl -s -L -o C:\Users\Public\Desktop\Winrar.exe https://www.rarlab.com/rar/winrar-x64-621.exe
+C:\Users\Public\Desktop\Winrar.exe /S
+del C:\Users\Public\Desktop\Winrar.exe
+del /f "C:\Users\Public\Desktop\Microsoft Edge.lnk"
+del /f "C:\Users\Public\Desktop\Firefox.lnk"
+del /f "C:\Users\Public\Desktop\Unity Hub.lnk"
